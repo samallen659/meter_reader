3a128eeade88e0677a87388b3f6e85c05ce67102
Sam Allen <samallen659@gmail.com>
2025-01-23 15:18:55 +0000

    reads consumption data from api

--- read.py
+++ read.py
@@ -1,4 +1,5 @@
 import os
+from enum import Enum
 
 import dotenv
 import requests
@@ -8,6 +9,11 @@ def exit_if_none(*args):
     for a in args:
         if a is None:
             os._exit(1)
+
+
+class Fuel(Enum):
+    GAS = 1
+    ELECTRICITY = 2
 
 
 def main():
@@ -31,13 +37,47 @@ def main():
         OCTOPUS_ACCOUNT_ID,
     )
 
-    get_octopus_data(
-        str(OCTOPUS_BASE_URL), str(OCTOPUS_API_KEY), str(OCTOPUS_ACCOUNT_ID)
+    # get_octopus_account(
+    #     str(OCTOPUS_BASE_URL), str(OCTOPUS_API_KEY), str(OCTOPUS_ACCOUNT_ID)
+    # )
+
+    # get_octopus_meter_point(
+    #     str(OCTOPUS_BASE_URL), str(OCTOPUS_API_KEY), str(OCTOPUS_ELC_MPAN)
+    # )
+    #
+    # get_octopus_meter_consumption(
+    #     str(OCTOPUS_BASE_URL),
+    #     str(OCTOPUS_API_KEY),
+    #     str(OCTOPUS_ELC_MPAN),
+    #     str(OCTOPUS_ELC_SERIAL),
+    #     Fuel.ELECTRICITY,
+    # )
+    get_octopus_meter_consumption(
+        str(OCTOPUS_BASE_URL),
+        str(OCTOPUS_API_KEY),
+        str(OCTOPUS_GAS_MPRN),
+        str(OCTOPUS_GAS_SERIAL),
+        Fuel.GAS,
     )
 
 
-def get_octopus_data(base_url: str, key: str, account_id: str):
+def get_octopus_account(base_url: str, key: str, account_id: str):
     url = f"{base_url}/accounts/{account_id}"
+    r = requests.get(url, auth=(key, ""))
+    print(r.json())
+
+
+def get_octopus_meter_point(base_url: str, key: str, mpan: str):
+    url = f"{base_url}electricity-meter-points/{mpan}"
+    r = requests.get(url, auth=(key, ""))
+    print(r.json())
+
+
+def get_octopus_meter_consumption(
+    base_url: str, key: str, mpan: str, serial: str, fuel: Fuel
+):
+    print(fuel)
+    url = f"{base_url}{"electricity" if fuel is Fuel.ELECTRICITY else "gas"}-meter-points/{mpan}/meters/{serial}/consumption"
     r = requests.get(url, auth=(key, ""))
     print(r.json())
 
