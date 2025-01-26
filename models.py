88478964ccbd1affe95bfd480c6498248c6403e1
Sam Allen <samallen659@gmail.com>
2025-01-26 19:37:50 +0000

    adds docstrings to models

--- models.py
+++ models.py
@@ -5,12 +5,16 @@ from typing import List
 
 
 class Fuel(Enum):
+    """Possible Fuel type for Octopus API requests"""
+
     GAS = 1
     ELECTRICITY = 2
 
 
 @dataclass
 class ConsumptionUnit:
+    """Single Unit of consumption contained within ConsumptionResponse"""
+
     consumption: int
     interval_start: datetime
     interval_end: datetime
@@ -18,6 +22,8 @@ class ConsumptionUnit:
 
 @dataclass
 class ConsumptionResponse:
+    """Structure of response from the Octopus API consumption Endpoint"""
+
     count: int
     next: None | str
     previous: None | str
